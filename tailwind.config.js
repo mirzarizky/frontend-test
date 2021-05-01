6e26eccbbbd4b2385e2b2f65c82b321c831bcd0e
Mirza Rizky <mirzarizky@gmail.com>
2021-05-01 15:55:27 +0700

    install symfony

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  purge: [
+      './templates/**/*.twig'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      }
+    },
+    extend: {
+      colors:{
+        primary: {
+          DEFAULT: '#007FDE',
+          lighter: '#008FEE'
+        }
+      },
+      fontFamily: {
+        'sans': ['Roboto', ...defaultTheme.fontFamily.sans],
+        'display': ['Montserrat', 'sans-serif']
+      }
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['responsive', 'dark', 'group-hover', 'focus-within', 'hover', 'focus', 'even'],
+    },
+  },
+  plugins: [],
+}
